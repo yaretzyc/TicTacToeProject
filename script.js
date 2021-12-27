9c281ee37ddec14eaf6657f086976a2d68928b31
Yaretzy Charo <yaretzycharo@my.unt.edu>
2021-12-27 14:53:50 -0600

    updated the css and html, added players div scores, centerd the tic tac toe, hover effect

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector('#bigBox');
+console.log(container);
+const restartButton = document.querySelector('#restart');
+console.log(restartButton);
+const winner = document.querySelector('#player-won-lost-tied');
+console.log(winner);
+/*
+player 1 be X and 
+player 2 be O
+
+For the players to put down their turn and 
+be recorded
+
+alert box if they win/lose/tie/restart
+
+*/
