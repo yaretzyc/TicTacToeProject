893160d5625ff1eb95bea78966e744577c98f9f7
Yaretzy Charo <yaretzycharo@my.unt.edu>
2021-12-28 15:33:53 -0600

    alerts when x wins need to do another if statement for when o wins

--- script.js
+++ script.js
@@ -13,11 +13,18 @@ console.log(winner);
 let nextPlayer = 'X'
 
 function clickedDiv(){
-    for (let i= 0; i<9; i++){
+    for (let i= 0; i < 9; i++){
         squareDivs[i].onclick = function (){
             let newPlayer = nextPlayer;
-            squareDivs[i].classList.remove('cells');  
+            squareDivs[i].classList.remove('cells'); 
+            squareDivs[i].onclick = function (){
+                return false
+            } 
             squareDivs[i].querySelector('p').innerHTML = newPlayer;
+
+            if(playerStatus()) return gameEnds(newPlayer);
+            // if (playerTie)
+            
                
             if (nextPlayer == 'X'){
                 nextPlayer = 'O'
@@ -29,11 +36,46 @@ function clickedDiv(){
 }
 clickedDiv();
 
-//
+function gameEnds(player){
+    alert('the winner is ' + player);
+}
+
+// function tiedGame(){
+//     let 
+// }
+
+function playerStatus(){
+
+    let winningComb = [
+        //rows
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        //columns
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        //diagonal
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    for (let i = 0; i < winningComb.length; i++){
+        const [x, y, z] = winningComb[i];
+        // console.log(winningComb[i])
+        console.log(x,y,z);
+        if(squareDivs[x].innerText === 'X' &&
+        squareDivs[y].innerText === 'X' &&
+        squareDivs[z].innerText === 'X'
+        )
+            { 
+            console.log('winner');
+            return true;
+
+        }
+    }
+    return false;
+}
 
 
-//set the winning combos
-//set when the games ends and alert the player
-//game ends when the player acheives the winning combo
-// score is reflected
 
