a8b3e5b1a6535659b3a300fe295b023375807d50
Yaretzy Charo <yaretzycharo@my.unt.edu>
2021-12-28 08:56:14 -0600

    made each square work with p element changing when clicking on the square div, need to alternate between players and let the player know if they won or lost and set the winning combos

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 const container = document.querySelector('#bigBox');
-console.log(container);
+// console.log(container);
+const squareDivs = document.querySelectorAll('[cell]');
+console.log(squareDivs);
+
 const restartButton = document.querySelector('#restart');
-console.log(restartButton);
+// console.log(restartButton);
+
 const winner = document.querySelector('#player-won-lost-tied');
 console.log(winner);
-/*
-player 1 be X and 
-player 2 be O
 
-For the players to put down their turn and 
-be recorded
+//
+// function clickedDiv(){
+//     for (let i= 0; i<9; i++){
+//     squareDivs[i].onclick = function () {
+//         console.log('cell index ' + [i]);
+//     }
+//     }
+// }
+// clickedDiv();
 
-alert box if they win/lose/tie/restart
 
-*/
+function clickedDiv(){
+    for (let i= 0; i<9; i++){
+    squareDivs[i].onclick = function (){
+        let newPlayer = 'X';
+        squareDivs[i].classList.remove('cells');  
+        
+        squareDivs[i].querySelector('p').innerHTML = newPlayer;
+    }
+
+
+    }
+}
+clickedDiv();
+
+//NEED TO alternate between x and o
+//set the winning combos
+//set when the games ends and alert the player
+//game ends when the player acheives the winning combo
+// score is reflected
