1470fa2e14acebf1e4f984b80b31e4a0989bbbbe
Yaretzy Charo <yaretzycharo@my.unt.edu>
2021-12-28 21:50:57 -0600

    cleaned it up and added my comments to show what each function does

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ const container = document.querySelector('#bigBox');
 const squareDivs = document.querySelectorAll('[cell]');
 // console.log(squareDivs);
 
-const restartButton = document.querySelector('#restart');
-// console.log(restartButton);
+/*
 
-const winner = document.querySelector('#player-won-lost-tied');
-// console.log(winner);
+Function clickedDiv() was created for whenever a square is clicked in the grid container it would show hover ability and set it to X or O. Then you player won't be able to click it again to change and once clicked can't hover over. 
 
+Also switches between player O and X with if/else statement.
 
-
+*/
 
 let nextPlayer = 'X'
 
@@ -24,9 +23,7 @@ function clickedDiv(){
             } 
             squareDivs[i].querySelector('p').innerHTML = newPlayer;
 
-            if(playerStatus()) return gameEnds(newPlayer);
-            // if (playerTie)
-            
+            if (playerStatus()) return gameEnds(newPlayer);
                
             if (nextPlayer == 'X'){
                 nextPlayer = 'O'
@@ -36,48 +33,58 @@ function clickedDiv(){
         }
     }
 }
+
 clickedDiv();
 
+/*
+
+function gameEnds(player) was created to alert if either player X or O is the winner as will be rendered by the playerStatus(); function
+
+*/
+
 function gameEnds(player){
-    alert('the winner is ' + player);
+    return alert('the winner is Player ' + player);
 }
 
-// function tiedGame(){
-//     let 
-// }
+let winningComb = [
+    //rows
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    //columns
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    //diagonal
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+/* 
+
+function playerStatus() grabs each winningComb array and assigns each index to x, y, and z. Then the 
+
+*/
 
 function playerStatus(){
-
-    let winningComb = [
-        //rows
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        //columns
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        //diagonal
-        [0, 4, 8],
-        [2, 4, 6]
-    ];
 
     for (let i = 0; i < winningComb.length; i++){
         const [x, y, z] = winningComb[i];
         // console.log(winningComb[i])
-        console.log(x,y,z);
+        // console.log(x,y,z);
         if(squareDivs[x].innerText === 'X' &&
         squareDivs[y].innerText === 'X' &&
         squareDivs[z].innerText === 'X'
         )
             { 
-            console.log('winner');
+            console.log('Player X is the winner');
             return true;
         }
         if (squareDivs[x].innerText === 'O' &&
         squareDivs[y].innerText === 'O' &&
         squareDivs[z].innerText === 'O')
         {
+            console.log('Player O is the winner');
             return true;
         }
     }
